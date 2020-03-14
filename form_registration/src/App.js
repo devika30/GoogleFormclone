9d8c6bd1641cb81f153236b88fb9722beb3df2e2
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-03-14 21:48:10 +0530

    update title+desc

--- form_registration/src/App.js
+++ form_registration/src/App.js
@@ -1,50 +1,90 @@
-import React,{Component} from 'react';
-import AddInput from './AddInput'
-
-class App extends Component{
-  state={
-    inputs:[],
-    title:'',
-    description:''
-  }
-  handleChange =(e)=>{
+import React, { Component } from 'react';
+import AddInput from './AddInput';
+import './index.css';
+class App extends Component {
+  state = {
+    inputs: [],
+    title: '',
+    description: ''
+  };
+  handleChange = e => {
     this.setState({
-        [e.target.id]:e.target.value
-        });
-   }
+      [e.target.id]: e.target.value
+    });
+  };
 
-  addinput=(form)=>{
+  addinput = form => {
     form.id = Math.random();
     let forms = [...this.state.inputs, form];
     this.setState({
-      inputs:forms
+      inputs: forms
     });
-  }
-  deleteinput=(id)=>{
+  };
+  deleteinput = id => {
     let forms = this.state.inputs.filter(form => {
-      return form.id !== id
+      return form.id !== id;
     });
     this.setState({
       inputs: forms
     });
-  }
+  };
 
+  handleTitle = e => {
+    e.preventDefault();
+    e.target.hidden = 'true';
+    this.setState({
+      title: this.titleInput.value
+    });
+  };
 
-  render()
-  {
-    console.log(this.state)
-    return(
-      <div className="App container">
-         <label htmlFor="question">Title:</label>
-      <input  className=" form-control mt-2" id="title" onChange={this.handleChange} value={this.state.title} required/>
-      <br/>
-      <label htmlFor="question">description:</label>
+  handleDesc = e => {
+    e.preventDefault();
+    e.target.hidden = 'true';
+    this.setState({
+      description: this.descInput.value
+    });
+  };
 
-      <input  className="form-control mt-2" id="description" onChange={this.handleChange} value={this.state.description} required/>
+  render() {
+    const title = this.state.title;
+    const description = this.state.description;
+    console.log(this.state);
+    return (
+      <div className='App container'>
+        <div className='card'>
+          <label htmlFor='question'>Title:{title}</label>
+          <form onSubmit={this.handleTitle}>
+            <input
+              ref={input => (this.titleInput = input)}
+              id='title'
+              required
+            />
+            <button type='submit' className='button'>
+              <i class='fas fa-check'></i>
+            </button>
+          </form>
+          <br />
+          <label htmlFor='question'>Description:{description}</label>
 
-      <AddInput addinput={this.addinput} inputs={this.state.inputs} deleteinput={this.deleteinput} />
-    </div>
-    )
+          <form onSubmit={this.handleDesc}>
+            <input
+              id='description'
+              ref={input => (this.descInput = input)}
+              required
+            />
+            <button type='submit' className='button'>
+              <i class='fas fa-check'></i>
+            </button>
+          </form>
+        </div>
+
+        <AddInput
+          addinput={this.addinput}
+          inputs={this.state.inputs}
+          deleteinput={this.deleteinput}
+        />
+      </div>
+    );
   }
 }
 
